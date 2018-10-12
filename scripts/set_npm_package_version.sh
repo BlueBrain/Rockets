8100fefa19c7e8339b0549e3b39ea25c3f83a597
Roland Groza <roland.groza@epfl.ch>
2018-10-12 11:14:29 +0200

    chore: fix js ci config

--- scripts/set_npm_package_version.sh
+++ scripts/set_npm_package_version.sh
@@ -2,13 +2,15 @@
 set -e
 
 REMOTE_VERSION=$(npm show rockets-client version)
+R_VER=${REMOTE_VERSION// /}
 # https://gist.github.com/DarrenN/8c6a5b969481725a4413
 LOCAL_VERSION=$(node -p "require('./js/package.json').version")
+L_VER=${LOCAL_VERSION// /}
 
-echo "Remote version: ${REMOTE_VERSION}"
-echo "Local version: ${LOCAL_VERSION}"
+echo "Remote version: ${R_VER}"
+echo "Local version: ${L_VER}"
 
-if [ "${REMOTE_VERSION}" != "${LOCAL_VERSION}" ]; then
-    export ROCKETS_CLIENT_VERSION=${LOCAL_VERSION}
-    echo "Set version: ${LOCAL_VERSION}"
+if [ "${R_VER}" != "${L_VER}" ]; then
+    export ROCKETS_CLIENT_VERSION=${L_VER}
+    echo "Set version: ${L_VER}"
 fi
